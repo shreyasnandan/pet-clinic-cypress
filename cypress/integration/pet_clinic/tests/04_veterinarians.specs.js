6dc044fb066e739882871294ba08d651a172a5d1
shreyasnandan <kiran.sinha@gartner.com>
2021-01-21 22:54:53 -0500

    Added verification for Vet links

--- cypress/integration/pet_clinic/tests/04_veterinarians.specs.js
+++ cypress/integration/pet_clinic/tests/04_veterinarians.specs.js
@@ -7,14 +7,24 @@ describe('Automation Test Suite - Fixtures', function () {
   var sel = new GetSelectors()
   var util = new CommonUtils()
 
-  it('Go to Veterinarians', function () {
-    
+  it('Go to Veterinarians', function () {    
     cy.visit('http://localhost:8080/')
     sel.getAllMenuItems()
       .then(function($lis){
         cy.log('Clicking Veterinarians menu')
         $lis.eq(2).find('span').click()
-      })   
+      })       
+  })
+
+  it('Verify Veterinarian Links', function(){
+    cy.get('table a').contains('View as XML').click()
+    cy.url().should('contain', '/vets.xml')
+
+    cy.go('back')
+    cy.url().should('contain','/vets.html')
+
+    cy.get('table a').contains('View as JSON').click()
+    cy.url().should('contain','/vets.json')
     
   })
 
